c55e22af8d7239c5134f729da38567c063cb3256
pitsanu <p.tongprasin@gmail.com>
2014-03-08 18:42:41 +0700

    complete chapter 1

---
+++ Quiz/Quiz/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Quiz
+//
+//  Created by Tom on 3/6/14.
+//  Copyright (c) 2014 The Big Mud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Quiz/Quiz/BNRQuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRQuizViewController.h
+//  Quiz
+//
+//  Created by Tom on 3/8/14.
+//  Copyright (c) 2014 The Big Mud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRQuizViewController : UIViewController
+
+@end
