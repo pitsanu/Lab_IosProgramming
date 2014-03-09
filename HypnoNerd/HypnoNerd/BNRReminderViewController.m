5825586e006d125ad5e187ddd42e11463bacb78c
pitsanu <p.tongprasin@gmail.com>
2014-03-10 05:37:30 +0700

    Add reminder view

---
+++ HypnoNerd/HypnoNerd/BNRReminderViewController.m
@@ -0,0 +1,13 @@
+//
+//  BNRReminderViewController.m
+//  HypnoNerd
+//
+//  Created by Tom on 3/10/14.
+//  Copyright (c) 2014 The Big Mud. All rights reserved.
+//
+
+#import "BNRReminderViewController.h"
+
+@implementation BNRReminderViewController
+
+@end
